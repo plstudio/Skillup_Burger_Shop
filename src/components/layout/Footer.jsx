9ef0a3e01f081945979241043e9024eb4889cdc1
plstudio <YOUR_NOREPLY@users.noreply.github.com>
2026-08-12 09:48:36 -0400

    Created footer with social media links

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,1 +1,27 @@
-// Write all the code here
+import React from "react";
+import { AiFillYoutube, AiFillInstagram } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2>BURGER SHOP</h2>
+        <p>We are trying to give you the best taste possible.</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+
+        https://www.youtube.com
+          <AiFillYoutube />
+        </a>
+
+        https://www.instagram.com
+          <AiFillInstagram />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
