ce38dc6bda9ed8a8f65c7b90bd08d903c49a703c
plstudio <YOUR_NOREPLY@users.noreply.github.com>
2026-08-12 10:12:35 -0400

    Created login and register tabs

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,1 +1,43 @@
-// Write all the code here
+import React, { useState } from 'react'
+
+const Login = () => {
+  const [isLogin, setIsLogin] = useState(true)
+
+  return (
+    <section className="login">
+      <div className="loginContainer">
+        <div className="tabs">
+          <button onClick={() => setIsLogin(true)}>Login</button>
+
+          <button onClick={() => setIsLogin(false)}>Register</button>
+        </div>
+
+        {isLogin ? (
+          <form>
+            <h2>Login</h2>
+
+            <input type="email" placeholder="Email" required />
+
+            <input type="password" placeholder="Password" required />
+
+            <button type="submit">Login</button>
+          </form>
+        ) : (
+          <form>
+            <h2>Register</h2>
+
+            <input type="text" placeholder="Name" required />
+
+            <input type="email" placeholder="Email" required />
+
+            <input type="password" placeholder="Password" required />
+
+            <button type="submit">Register</button>
+          </form>
+        )}
+      </div>
+    </section>
+  )
+}
+
+export default Login
