ce6dd1f6654d049b70ed15aceab5e910d6656d86
plstudio <YOUR_NOREPLY@users.noreply.github.com>
2026-08-12 10:07:03 -0400

    Created About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,1 +1,21 @@
-// Write all the code here
+import React from 'react'
+import founder from '../../assets/OIP.jpg'
+
+const About = () => {
+  return (
+    <section className="about">
+      <div>
+        {founder}
+
+        <h1>About Us</h1>
+
+        <p>
+          Welcome to Burger Shop. We are committed to serving fresh,
+          high-quality burgers with outstanding customer service.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
