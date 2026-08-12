579ce024d6d9f48cba48342ecfbbd146e682497b
plstudio <YOUR_NOREPLY@users.noreply.github.com>
2026-08-12 09:45:55 -0400

    Created Home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,25 @@
-// Write all the code here
+import React from "react";
+import Founder from "./Founder";
+import Menu from "./Menu";
 
+const Home = () => {
+  return (
+    <>
+      <section className="home">
+        <div>
+          <h1>BURGER SHOP</h1>
+          <p>Give Yourself a New Taste</p>
 
+          #menu
+            Explore Menu
+          </a>
+        </div>
+      </section>
 
+      <Founder />
+      <Menu />
+    </>
+  );
+};
+
+export default Home;
