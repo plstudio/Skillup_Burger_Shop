2086e98390268896238046d1eeec373432160f82
plstudio <YOUR_NOREPLY@users.noreply.github.com>
2026-08-12 10:25:11 -0400

    Created cart page with quantity controls

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import burger1 from "../../assets/burger1.png";
-import burger2 from "../../assets/burger2.png";
-// import burger3 here
+import React from 'react'
+import { Link } from 'react-router-dom'
+import burger1 from '../../assets/burger1.png'
+import burger2 from '../../assets/burger2.png'
+import burger3 from '../../assets/burger3.png'
 
 const CartItem = ({ value, title, img, increment, decrement }) => (
   <div className="cartItem">
@@ -17,36 +17,39 @@ const CartItem = ({ value, title, img, increment, decrement }) => (
       <button onClick={increment}>+</button>
     </div>
   </div>
-);
+)
 
 const Cart = () => {
-  const increment = (item) => {};
+  const increment = (item) => {}
 
-  const decrement = (item) => {};
+  const decrement = (item) => {}
 
   return (
     <section className="cart">
       <main>
         <CartItem
-          title={"Cheese Burger"}
+          title={'Cheese Burger'}
           img={burger1}
           value={0}
           increment={() => increment(1)}
-
-        // Add the function for decrementing the order by 1 
-       
+          decrement={() => decrement(1)}
         />
         <CartItem
-          title={"Veg Cheese Burger"}
+          title={'Veg Cheese Burger'}
           img={burger2}
           value={0}
           increment={() => increment(2)}
-        // Add the function for decrementing the order by 2
-       
+          decrement={() => decrement(2)}
+        />
+        <CartItem
+          title={'Cheese Burger with French'}
+          img={burger3}
+          value={0}
+          increment={() => increment(3)}
+          decrement={() => decrement(3)}
         />
 
         {/* Fill up the code for Cheese Burger similarly */}
-       
 
         <article>
           <div>
@@ -60,7 +63,7 @@ const Cart = () => {
           <div>
             <h4>Shipping Charges</h4>
             <p>₹{200}</p>
-          </div>{" "}
+          </div>{' '}
           <div>
             <h4>Total</h4>
             <p>₹{2000 + 2000 * 0.18 + 200}</p>
@@ -69,7 +72,7 @@ const Cart = () => {
         </article>
       </main>
     </section>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart
